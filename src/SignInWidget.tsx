e29f4d87cf8797322b115e7f102a541b2426e08f
lim0n3 <simonestaffa96@gmail.com>
2025-06-02 14:18:37 +0200

    fix button

--- src/SignInWidget.tsx
+++ src/SignInWidget.tsx
@@ -1,11 +1,12 @@
-import { SignInButton, AuthKitProvider } from "@farcaster/auth-kit";
+import { AuthKitProvider, SignInButton } from "@farcaster/auth-kit";
 import "@farcaster/auth-kit/styles.css";
 import { JsonRpcProvider } from "ethers";
 
 const config = {
-  domain: "https://spinning-sphere-788554.framer.app/",
-  siweUri: "https://spinning-sphere-788554.framer.app/",
+  domain: "spinning-sphere-788554.framer.app",
+  siweUri: "https://spinning-sphere-788554.framer.app",
   provider: new JsonRpcProvider(undefined, 10),
+  relay: "https://relay.farcaster.xyz",
 };
 
 const SignInWidget = () => {
@@ -13,14 +14,17 @@ const SignInWidget = () => {
   return (
     <AuthKitProvider config={config}>
       <div style={{ padding: "1rem", fontFamily: "sans-serif" }}>
-      <SignInButton
-        nonce={nonce}
-        onSuccess={({ fid, username }) => {
-          console.log(`Signed in as ${username} (fid: ${fid})`);
-          window.parent.postMessage(
-            { type: "farcaster-auth-success", fid, username },
-            "*"
-          );
+        <SignInButton
+          nonce={nonce}
+          onError={(error) => {
+            console.error(error);
+          }}
+          onSuccess={({ fid, username }) => {
+            console.log(`Signed in as ${username} (fid: ${fid})`);
+            window.parent.postMessage(
+              { type: "farcaster-auth-success", fid, username },
+              "*"
+            );
           }}
         />
       </div>
